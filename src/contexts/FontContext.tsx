e34688fcd965984fda5212ed0aa724c5d8330fc9
seniorsam9 <rxrld050@hotmail.com>
2025-11-23 22:39:58 +0300

    FontContext

--- src/contexts/FontContext.tsx
+++ src/contexts/FontContext.tsx
@@ -1,17 +1,56 @@
-import { useFonts } from "expo-font";
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useContext } from "react";
+import {
+  useFonts as useInterFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+} from "@expo-google-fonts/inter";
+import {
+  useFonts as useNotoFonts,
+  NotoSansArabic_400Regular,
+  NotoSansArabic_600SemiBold,
+} from "@expo-google-fonts/noto-sans-arabic";
 
-export const FontContext = createContext<{} | null>(null);
+type FontContextType = {
+  fontsLoaded: boolean;
+  fonts: {
+    en: { regular: string; semiBold: string };
+    ar: { regular: string; semiBold: string };
+  };
+};
+
+const FontContext = createContext<FontContextType>({
+  fontsLoaded: false,
+  fonts: {
+    en: { regular: "", semiBold: "" },
+    ar: { regular: "", semiBold: "" },
+  },
+});
 
 export default function FontProvider({ children }: { children: ReactNode }) {
-  const [isFontLoaded] = useFonts({
-    ArFont: require(""), // TBS
-    EnFont: require(""), // TBS
+  // load both fonts
+  const [interLoaded] = useInterFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+  });
+  const [notoLoaded] = useNotoFonts({
+    NotoSansArabic_400Regular,
+    NotoSansArabic_600SemiBold,
   });
 
-  if (!isFontLoaded) {
-    return null;
-  }
+  const fontsLoaded = interLoaded && notoLoaded;
+  const fonts = {
+    en: { regular: "Inter_400Regular", semiBold: "Inter_600SemiBold" },
+    ar: {
+      regular: "NotoSansArabic_400Regular",
+      semiBold: "NotoSansArabic_600SemiBold",
+    },
+  };
 
-  return <FontContext.Provider value={{}}>{children}</FontContext.Provider>;
+  if (!fontsLoaded) return null;
+
+  return (
+    <FontContext.Provider value={{ fontsLoaded, fonts }}>
+      {children}
+    </FontContext.Provider>
+  );
 }
